80679b9633b38bd2f8855c80bc5a34d38fc8c9aa
TechGrizzlee <55645864+TechGrizzlee@users.noreply.github.com>
2024-08-17 16:19:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+
+cnx = st.connection('snowflake')
+session = cnx.session()
 
 # Write directly to the app
 st.title("Customize Your Smoothie!")
@@ -11,7 +14,7 @@ st.write(
 )
 
 # Get the current credentials
-session = get_active_session()
+
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
